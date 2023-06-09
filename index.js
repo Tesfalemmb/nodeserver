aaf008914a8d5ceaa4c116f6cbacc94fbaca7bfb
Tesfalemmb <tesfalemmehari2@gmail.com>
2023-06-09 09:08:20 +0200

    name added

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const app = express();
 const port = 3070;
 
 app.get('/', (req, res) => {
-  res.send("TODO: Write your first and last name here - Write your student number here: On your way to pass the lab test... focus on submitting the right files on time!"); // e.g. Mario Rossi - 123456
+  res.send("Tesfalem Mehari Berhe ID 626677- Write your student number here: On your way to pass the lab test... focus on submitting the right files on time!"); // e.g. Mario Rossi - 123456
 });
-
+        
 
 app.listen(port, (err) => {
   console.log(`running server on from port:::::::${port}`);
